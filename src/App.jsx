480d01aa3f4b28363afa60fca7f9386466d7c437
Ibiere William-west <ibiwilliamwest@gmail.com>
2024-02-26 12:15:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header'
+
+function App() {
+  const [count, setCount] = useState(0)
+   console.log(count)
+  return (
+    <div>
+      <div className='color'>Hello world</div>
+
+      <Header/>
+    </div>
+  
+  
+  )
+}
+
+export default App
